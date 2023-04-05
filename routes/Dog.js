4081748be656e2a521a58b87b9d4a6b51e0d9018
Jitender-Singh-NWM <jithu.cbit@gmail.com>
2023-04-05 00:50:01 -0500

    update

--- routes/Dog.js
+++ routes/Dog.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Search Results Dog' });
+  res.render('Dog', { title: 'Search Results Dog' });
 });
 
 module.exports = router;
