c16cd0ab9ab16b5b476ecdafb049ab1779c9b1c6
Jitender-Singh-NWM <jithu.cbit@gmail.com>
2023-04-05 07:58:18 -0500

    update

--- routes/board.js
+++ routes/board.js
@@ -4,10 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     let query = req.query;
+    console.log('in board');
     console.log(`rows ${query.rows}`);
     console.log(`cols ${query.cols}`);
 
-  res.render('board', { title: 'Board Display',query: query });
+  res.render('board', { title: 'Board',query: query });
 });
 
 module.exports = router;
